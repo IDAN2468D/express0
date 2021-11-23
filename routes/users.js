a45659de68dfef80e46ad57a0dd690e3bc44d414
Idan Kazam <48420836+IDAN2468D@users.noreply.github.com>
2021-11-23 12:44:48 +0200

    Update

--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,9 @@ const express = require("express");
 const { UsersModel, validUser } = require("../models/usersModel")
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    UsersModel.find()
-        .then((shop) => res.send(shop))
-        .catch((error) => {
-            res.status(500).send("Something went wrong");
-        })
+router.get("/", async (req, res) => {
+    let data = await UsersModel.find(req.body);
+    res.json(data);
 })
 
 router.post("/user", async (req, res) => {
