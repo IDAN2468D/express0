74f18f00ec27a30743f1e1c7308409953aff6a9d
Idan Kazam <48420836+IDAN2468D@users.noreply.github.com>
2021-11-27 01:10:42 +0200

    Update

--- db/mongoConnect.js
+++ db/mongoConnect.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/express0',
+require("dotenv").config();
+
+mongoose.connect(process.env.MONGO_URI,
     { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
